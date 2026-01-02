5c6d03070be85ed1f73f3714d4bb6ebcc1b4fa3f
Vlad <caraseli02@gmail.com>
2026-01-02 14:42:40 +0100

    fix: guard tooltip provider on ssr to avoid ce crash

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,28 @@
 <template>
-  <UiTooltipProvider :delay-duration="300">
-    <div class="min-h-screen flex flex-col">
-      <LayoutAppHeader />
-      <main class="flex-1 bg-white dark:bg-gray-950 text-gray-900 dark:text-white pb-16 md:pb-0">
-        <slot></slot>
-      </main>
-      <LayoutAppFooter />
-      <!-- Bottom Navigation for Mobile -->
-      <LayoutBottomNav />
-      <ClientOnly>
-        <!-- Sonner toaster (shadcn-vue) -->
-        <Sonner
-          position="top-right"
-          :rich-colors="true"
-        />
-        <!-- PWA Components -->
-        <MobilePWAInstallPrompt />
-        <MobilePWAUpdatePrompt />
-        <MobileOfflineIndicator />
-      </ClientOnly>
-    </div>
-  </UiTooltipProvider>
+  <ClientOnly>
+    <UiTooltipProvider :delay-duration="300">
+      <div class="min-h-screen flex flex-col">
+        <LayoutAppHeader />
+        <main class="flex-1 bg-white dark:bg-gray-950 text-gray-900 dark:text-white pb-16 md:pb-0">
+          <slot></slot>
+        </main>
+        <LayoutAppFooter />
+        <!-- Bottom Navigation for Mobile -->
+        <LayoutBottomNav />
+        <ClientOnly>
+          <!-- Sonner toaster (shadcn-vue) -->
+          <Sonner
+            position="top-right"
+            :rich-colors="true"
+          />
+          <!-- PWA Components -->
+          <MobilePWAInstallPrompt />
+          <MobilePWAUpdatePrompt />
+          <MobileOfflineIndicator />
+        </ClientOnly>
+      </div>
+    </UiTooltipProvider>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
